534fa701c1438bcfcc1414c29227e85abe278bd9
SergeyMell <sergey.mell@agilie.com>
2016-06-19 02:51:18 +0300

    article file display customization

--- app/admin/article_file.rb
+++ app/admin/article_file.rb
@@ -1,6 +1,25 @@
 ActiveAdmin.register ArticleFile do
 
-  permit_params :file
+  permit_params :file, :article_id
+
+  index do |t|
+    t.column :id
+    t.column :article
+    t.column 'File' do |article_file|
+      article_file.file.exists? ? link_to(article_file.file.original_filename, article_file.file.url, :target => '_blank') : ''
+    end
+    actions
+  end
+
+  show do |article_file|
+    attributes_table do
+      row :id
+      row :article
+      row :file do
+        article_file.file.exists? ? link_to(article_file.file.original_filename, article_file.file.url, :target => '_blank') : ''
+      end
+    end
+  end
 
   form do |f|
     f.inputs do
