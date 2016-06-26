daccca2a1f478ccbe47845e0c369f0e1d1185eee
SergeyMell <sergey.mell@agilie.com>
2016-06-27 00:05:08 +0300

    Implemented modals for article information showing

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :title
 
+  before_save :update_connections
+
   belongs_to :author
 
   has_many :article_files
@@ -11,4 +13,8 @@ class Article < ActiveRecord::Base
   has_and_belongs_to_many :sections
   has_and_belongs_to_many :subsections
 
+  def update_connections
+    puts 'article data'
+    puts self.chapters.inspect
+  end
 end
