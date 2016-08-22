ad062f1955296bac1679a9160c4cbed2389cac89
SergeyMell <sergey.mell@agilie.com>
2016-08-22 23:21:01 +0300

    Implemented wrappers for create/update functionality

--- app/controllers/bookmark_groups_controller.rb
+++ app/controllers/bookmark_groups_controller.rb
@@ -9,4 +9,25 @@ class BookmarkGroupsController < ApplicationController
     # end
   end
 
+  def create
+    BookmarkGroup.create!(permit_params)
+    render json: {success: 0}
+  rescue => e
+    render status: 500, json: {error: e.message}
+  end
+
+  def update
+    bookmark_group = BookmarkGroup.find(params[:id])
+    bookmark_group.update!(permit_params)
+    render json: {success: 0}
+  rescue => e
+    render status: 500, json: {error: e.message}
+  end
+
+  private
+
+  def permit_params
+    params.require(:bookmark_group).permit(:title, :user_id, :bookmark_group_id)
+  end
+
 end
